e9b27dea3afe287eba67de0885a2e8ccd987e89c
busolienzo <enzobusoli@gmail.com>
2025-04-26 15:02:56 -0300

    Add files via upload

---
+++ id.js
@@ -0,0 +1,9 @@
+function mostrarInfo() {
+    const info = document.getElementById('info-extra');
+    if (info.style.display === 'none') {
+      info.style.display = 'block';
+    } else {
+      info.style.display = 'none';
+    }
+  }
+  
